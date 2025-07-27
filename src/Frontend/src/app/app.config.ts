684592cdf49123be7044735d4c4fedb3276f6f61
Eslaam <eslaam95m@gmail.com>
2025-07-27 18:51:09 +0200

    fix producttype

--- src/Frontend/src/app/app.config.ts
+++ src/Frontend/src/app/app.config.ts
@@ -6,7 +6,6 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth-interceptor';
@@ -16,7 +15,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])
-    )
-  ]
+    provideHttpClient(withInterceptors([authInterceptor])),
+  ],
 };
