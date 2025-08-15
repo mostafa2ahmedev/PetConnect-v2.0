d9e30fa43f7ba1683aba3cc29ef86372341f9cb0
BahaaMekkawy <Bahaa.elmekkawy@gmail.com>
2025-08-15 09:44:55 +0300

    adding-reviews-table

---
+++ src/Backend/PetConnect.DAL/Data/Models/Review.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetConnect.DAL.Data.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string? DoctorId { get; set; }
+        public Doctor? Doctor { get; set; }
+
+        public string? CustomerId { get; set; }
+        public Customer? Customer { get; set; }
+
+        public int Rating { get; set; }
+        public string? ReviewText { get; set; }
+        public DateTime ReviewDate { get; set; }
+
+    }
+}
